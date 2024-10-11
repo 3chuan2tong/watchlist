4f87961c1a697d7790c009371facc803b792d336
sanchuan <1421867479@qq.com>
2024-10-11 16:49:28 +0800

    Add index page

--- hello.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-@app.route('/index')
-@app.route('/home')
-def index():
-    return '<h1>"Hello, Totoro!"</h1><img src="http://helloflask.com/totoro.gif">'
